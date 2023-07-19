0ff4b98d881fd5ace4c12aa7e28766e8a1bcebb4
David-Inkheart <okoliedave1@gmail.com>
2023-07-19 16:07:33 +0100

    jest setup

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(test|spec).ts'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  moduleDirectories: ['node_modules', 'src'],
+  clearMocks: true,
+  resetMocks: true,
+};
+
+export default config;
